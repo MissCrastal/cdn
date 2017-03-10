3072d642b644aff252cde535441a7550eacbedfc
骆代朋 <abc237274727@126.com>
2017-03-10 18:01:48 +0800

    修改最短路径(DijKstra)法中选择下一个节点的过程

--- com/cacheserverdeploy/deploy/Deploy.java
+++ com/cacheserverdeploy/deploy/Deploy.java
@@ -3,6 +3,7 @@ package com.cacheserverdeploy.deploy;
 import java.util.Arrays;
 
 import com.sun.org.apache.bcel.internal.generic.Type;
+import com.sun.org.apache.xpath.internal.operations.Bool;
 
 public class Deploy {
 	/**
@@ -25,20 +26,30 @@ public class Deploy {
 			System.out.println(arr[i]);
 		}
 	}
+	
+	public static boolean findOpt(int[] selectOpt){
+		
+		for (int i=0;i<selectOpt.length;i++){
+			if (selectOpt[i]==0){
+				return true;
+			}
+		}
+		return false;
+	}
 
 	public static int[] getShortestDistance(int[][] arr, int startNode) {
-		final int MAX_VALUE = 2147483647;
-		// int[][] arr={{0,6,3,MAX_VALUE,MAX_VALUE,MAX_VALUE},
-		// {6,0,2,5,MAX_VALUE,MAX_VALUE,MAX_VALUE},
-		// {3,2,0,3,4,MAX_VALUE},
-		// {MAX_VALUE,5,3,0,2,3},
-		// {MAX_VALUE,MAX_VALUE,4,2,0,5},
-		// {MAX_VALUE,MAX_VALUE,MAX_VALUE,3,5,0}};
+		final int MAX_VALUE = 65536;
+//		 int[][] arr={{0,6,3,MAX_VALUE,MAX_VALUE,MAX_VALUE},
+//		 {6,0,2,5,MAX_VALUE,MAX_VALUE,MAX_VALUE},
+//		 {3,2,0,3,4,MAX_VALUE},
+//		 {MAX_VALUE,5,3,0,2,3},
+//		 {MAX_VALUE,MAX_VALUE,4,2,0,5},
+//		 {MAX_VALUE,MAX_VALUE,MAX_VALUE,3,5,0}};
 
 		int nodeNum = arr.length; // 网络节点的个数
 		int[][] distance = arr; // 网络节点之间
 
-		// int startNode = 0; // 起始节点ID
+//		 int startNode = 0; // 起始节点ID
 		int[] shortestDis = arr[startNode]; // 当前起始节点到各节点的最短距离
 
 		int[] selectOpt = new int[nodeNum]; // 节点选择标示集，选择的值为1，未选择的值为0
@@ -52,13 +63,14 @@ public class Deploy {
 		int count = 0; // 循环次数
 
 		while (true) {
+			System.out.print("Current node:"+currentNode+" ");
 			count = count + 1;
 			int currentShortestDis = shortestDis[currentNode]; // 起始节点到当前节点的最短距离
 			selectOpt[currentNode] = 1; // 选择的节点标记为1
 			int nextNode = currentNode; // 下一个节点
 			int shortestPath = MAX_VALUE; // 当前节点与下一个节点间的最短距离
 			for (int i = 0; i < nodeNum; i++) {
-				if (selectOpt[i] == 0 && distance[currentNode][i] < MAX_VALUE) {
+				if (selectOpt[i] == 0) {
 
 					// 如果起始节点到某个节点出现更短的路线，更新起始节点到该节点的距离
 					int newDis = currentShortestDis + distance[currentNode][i];
@@ -67,7 +79,7 @@ public class Deploy {
 					}
 
 					// 选择作为当前节点的下一个节点
-					if (distance[currentNode][i] < shortestPath) {
+					if (shortestDis[i] < shortestPath) {
 						shortestPath = distance[currentNode][i];
 						nextNode = i;
 					}
@@ -75,25 +87,23 @@ public class Deploy {
 				}
 			}
 
-			if (nextNode == currentNode) {
-				System.out
-						.println("开始节点:" + startNode + "\t" + "循环次数：" + count);
+			if (findOpt(selectOpt)==false) {
+				
+				System.out.println("开始节点:" + startNode + "\t" + "循环次数：" + count);
 				break;
 			}
-
+			
+			System.out.println("NextNode:"+nextNode);
+			
 			currentNode = nextNode;
 
 		}
-
-		// for (int i = 0; i < nodeNum; i++) {
-		// System.out.println(shortestDis[i] + " ");
-		// }
 
 		return shortestDis;
 	}
 
 	public static int[][] getNodeArr(String[] LinkInfo, int NetNodeNum) {
-		final int MAX_VALUE = 2147483647;
+		final int MAX_VALUE = 65536;
 		int[][] nodeArr = new int[NetNodeNum][NetNodeNum];
 
 		for (int i = 0; i < NetNodeNum; i++) {
@@ -120,35 +130,47 @@ public class Deploy {
 		return nodeArr;
 	}
 
+	public static void getNodeInfo(String[] LinkInfo){
+		for (int i = 0; i < LinkInfo.length; i++) {
+			String[] NodeInfo = LinkInfo[i].split(" ");
+			System.out.println(NodeInfo[0]+" "+NodeInfo[1]+" "+NodeInfo[3]);
+		}
+	}
+	
+	
 	public static String[] deployServer(String[] graphContent) {
 		/** do your work here **/
 
 		// printArr(graphContent);
 		String[] NetInfo = graphContent[0].split(" ");
-		Integer NetNodeNum = Integer.parseInt(NetInfo[0]); // 网络节点数量
-		Integer NetLinkNum = Integer.parseInt(NetInfo[1]); // 网络链路数量
-		Integer ConsumeNodeNum = Integer.parseInt(NetInfo[2]); // 消费节点数量
-		Integer ServerCost = Integer
-				.parseInt((graphContent[2].split("\r\n"))[0]); // 视频内容服务器部署成本
-		String[] LinkInfo = Arrays.copyOfRange(graphContent, 4, 4 + NetLinkNum); // 链路信息
+		Integer NetNodeNum = Integer.parseInt(NetInfo[0]); 			// 网络节点数量
+		Integer NetLinkNum = Integer.parseInt(NetInfo[1]); 				// 网络链路数量
+		Integer ConsumeNodeNum = Integer.parseInt(NetInfo[2]);	// 消费节点数量
+		Integer ServerCost = Integer.parseInt((graphContent[2].split("\r\n"))[0]);		// 视频内容服务器部署成本
+		String[] LinkInfo = Arrays.copyOfRange(graphContent, 4, 4 + NetLinkNum);	// 链路信息
 		String[] ConsumeInfo = Arrays.copyOfRange(graphContent, 5 + NetLinkNum,
-				5 + NetLinkNum + ConsumeNodeNum); // 消费节点信息
+				5 + NetLinkNum + ConsumeNodeNum); 	// 消费节点信息
 
 		int[][] nodeArr = getNodeArr(LinkInfo, NetNodeNum);
 
-		// for (int i = 0; i < NetNodeNum; i++) {
-		// for (int j = 0; j < NetNodeNum; j++) {
-		// System.out.print(nodeArr[i][j]+" ");
-		// }
-		// System.out.println("");
-		// }
-
+//		for (int i = 0; i < NetNodeNum; i++) {
+//			for (int j = 0; j < NetNodeNum; j++) {
+//				System.out.print(nodeArr[i][j] + " ");
+//			}
+//			System.out.println("");
+//		}
+		
+//		getNodeInfo(LinkInfo);
+		
+		System.out.println("start");
 		int[] result = getShortestDistance(nodeArr, 0);
-
+		
+		System.out.println(result.length);
 		for (int i = 0; i < result.length; i++) {
 			System.out.print(result[i] + " ");
 		}
-
+		System.out.println("end");
+		
 		return new String[] { "17", "\r\n", "0 8 0 20" };
 	}
 
